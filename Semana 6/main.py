b5cf511142ba8a6e1dd47d5f4f154cae09a573c0
justog220 <justogarciapc@gmail.com>
2024-05-01 11:11:58 -0300

    Reorganizo

--- Semana 6/main.py
+++ Semana 6/main.py
@@ -1,45 +1,46 @@
 import math
-class AutomataMiocardico:
-	def __init__(self):
-		self.estado = 'R'
-		self.E = -90
-		self.UR = 90
-		self.UP = 120
-
-	def auto_excitar(self):
-		self.estado = 'E'
-	
-	def calc_E(self, vec_1_E, vec_2_E, vec_3_E, vec_4_E):
-		vecinos = [vec_1_E, vec_2_E, vec_3_E, vec_4_E]
-		vecinos = [i - self.E for i in vecinos] 
-		CC_k = sum(vecinos)
-		# print(CC_k)
-		if self.estado == 'R':
-			if CC_k > self.UR:
-				self.estado = 'E'
-		elif self.estado == 'E':
-			self.E += 60
-			if self.E >= 30:
-				self.estado = 'PRA'
-		elif self.estado == 'PRA':
-			self.E = self.E * math.e**(-0.04)
-			if self.E <= 1/1000:
-				self.estado = 'PRR'
-		elif self.estado == 'PRR':
-			if CC_k > self.UP:
-				self.E = 'E'
-				self.E = self.E + 60
-			else:
-				self.E = self.E - 1
-				if self.E <= -90:
-					self.E = -90
-					self.estado = 'R'
-                              
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import copy
-fig, ax = plt.subplots()
+class AutomataMiocardico:
+    def __init__(self):
+        self.estado = 'R'
+        self.E = -90
+        self.UR = 90
+        self.UP = 120
+
+    def auto_excitar(self):
+        self.estado = 'E'
+
+    def calc_E(self, vec_1_E, vec_2_E, vec_3_E, vec_4_E):
+        vecinos = [vec_1_E, vec_2_E, vec_3_E, vec_4_E]
+        vecinos = [i - self.E for i in vecinos] 
+        CC_k = sum(vecinos)
+        # print(CC_k)
+        if self.estado == 'R':
+            if CC_k > self.UR:
+                self.estado = 'E'
+        elif self.estado == 'E':
+            self.E += 60
+            if self.E >= 30:
+                self.estado = 'PRA'
+        elif self.estado == 'PRA':
+        # self.E = self.E * math.e**(-0.04)
+            self.E = self.E * 0.96
+            if self.E <= 1/1000:
+                self.estado = 'PRR'
+        elif self.estado == 'PRR':
+            if CC_k > self.UP:
+                self.E = 'E'
+                self.E = self.E + 60
+            else:
+                self.E = self.E - 1
+                if self.E <= -90:
+                    self.E = -90
+                    self.estado = 'R'
+                                    
+# fig, ax = plt.subplots()
 
 filas = 100
 columnas = 100
@@ -54,18 +55,25 @@ if columnas%2 == 0:
 centro = (int(filas/2), int(columnas/2))
 
 
-segundos = 2
-ts = np.arange(0, segundos, 0.001)
+segundos = 5
+ts = np.arange(0, segundos, 0.01)
 
 artists = []
-def update(t):
-    global G
-    # G_futura = G
-    G_futura = copy.deepcopy(G)
+
+G = [[AutomataMiocardico() for j in range(columnas)] for i in range(filas)]
+
+vmin, vmax = -90, 50
+# im = ax.imshow([[G[i][j].E for j in range(columnas)] for i in range(filas)])
+im = plt.imshow([[G[i][j].E for j in range(columnas)] for i in range(filas)], vmin=vmin, vmax=vmax)
+
+for t in ts:
+    print(t)
     if (t-0.5)%1 == 0:
         G[centro[0]][centro[1]].auto_excitar()
+    
+    G_futura = copy.deepcopy(G)
     for f in range(filas):
-        for c in range(columnas):
+        for c in range(columnas):            
             if f == 0:
                 if c == 0:
                     G_futura[f][c].calc_E(G[f][c].E, G[f][c+1].E, G[f+1][c].E, G[f][columnas-1].E)
@@ -97,24 +105,12 @@ def update(t):
             else:
                 G_futura[f][c].calc_E(G[f-1][c].E, G[f][c+1].E, G[f+1][c].E, G[f][c-1].E)        
                     
-            G = copy.deepcopy(G_futura)
-    im.set_array([[G[i][j].E for j in range(columnas)] for i in range(filas)])
+                    
+    G = copy.deepcopy(G_futura)
 
-    return [im]
+    im.set_data([[G[i][j].E for j in range(columnas)] for i in range(filas)])
 
-G = [[AutomataMiocardico() for j in range(columnas)] for i in range(filas)]
+    plt.draw()
+    plt.pause(0.01)
 
-im = ax.imshow([[G[i][j].E for j in range(columnas)] for i in range(filas)])
 
-plt.colorbar(im)
-
-ani = FuncAnimation(fig, update, frames=ts, interval=1)
-
-import matplotlib
-matplotlib.rcParams['animation.embed_limit'] = 100
-
-ani_html = ani.to_jshtml()
-with open('animacion.html', 'w') as f:
-    f.write(ani_html)
-# ani.save('animacion.gif', writer='ffmpeg', fps=30)
-# plt.show()
